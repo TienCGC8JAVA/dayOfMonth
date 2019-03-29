e70141fdd91ab188edd1663d46d715a1606446bf
TienCGC8JAVA <nguyenmanhtien08081994@gmail.com>
2019-03-29 12:03:59 +0700

    Ngày trong tháng

---
+++ DayOfMonth.js
@@ -0,0 +1,29 @@
+function dayOfMoth()
+{
+    let month = parseInt(document.getElementById("month").value);
+    switch (month)
+    {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            document.write("Tháng " + month + " có 31 ngày");
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            document.write("Tháng " + month + " có 30 ngày");
+            break;
+        case 2:
+            document.write("Tháng " + month + " có 28 hoặc 29 ngày");
+            break;
+        default:
+            document.write(" Một năm chỉ có 12 tháng! Bạn đã nhập sai!");
+            break;
+
+    }
+}
